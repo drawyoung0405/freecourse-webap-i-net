cf2d1971a279c73344e543101c9e68dacaf51b95
Vo Nhu Hoa <hoavn0934813785@gmail.com>
2023-12-30 02:07:19 +0700

    Fluent API Update

---
+++ MyWebApiBasic/MyWebApiBasic/Data/DonHangChiTiet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyWebApiBasic.Data
+{
+    public class DonHangChiTiet
+    {
+        public Guid MaHh { get; set; }
+        public Guid MaDonHang { get; set; }
+        public int SoLuong { get; set; }
+        public double DonGia { get; set; }
+        public byte GiamGia { get; set; }
+
+        public DonHang DonHang { get; set; }
+        public HangHoa HangHoa { get; set; }
+
+    }
+}
